4f9af844b64a3053b41e13d5cbbbb71045201105
Maurice Fiorani <mauricef97@gmail.com>
2020-03-18 18:08:40 +0000

    modified code

--- lib/champions_league.rb
+++ lib/champions_league.rb
@@ -1,3 +1,6 @@
+require "open-uri"
+require "nokogiri"
+require "pry"
 require_relative "./champions_league/version"
 require_relative './champions_league/cli'
 require_relative './champions_league/teams'
--- lib/champions_league/teams.rb
+++ lib/champions_league/teams.rb
@@ -2,37 +2,22 @@ class ChampionsLeague::Teams
   attr_accessor :name, :wins, :losses, :draws, :total_points
   
   def self.all 
-   
-    team_1 = self.new 
-    team_1.name = "Liverpool"
-    team_1.wins = "234"
-    team_1.losses = "123"
-    team_1.draws = "214"
-    team_1.total_points = "23"
+    self.scrape_teams
+  end
+  
+  def self.scrape_teams 
+    teams = []
     
-    team_2 = self.new 
-    team_2.name = "Manchester City"
-    team_2.wins = "234"
-    team_2.losses = "123"
-    team_2.draws = "214"
-    team_2.total_points = "23"
+    teams << self.scrape_premier
     
-    team_3 = self.new 
-    team_3.name = "Leicester City"
-    team_3.wins = "234"
-    team_3.losses = "123"
-    team_3.draws = "214"
-    team_3.total_points = "23"
-    
-    team_4 = self.new 
-    team_4.name = "Chelsea"
-    team_4.wins = "234"
-    team_4.losses = "123"
-    team_4.draws = "214"
-    team_4.total_points = "23"
-    
-    [team_1, team_2, team_3, team_4]
-    
+    teams
+  end
+  
+  def self.scrape_premier
+    doc = Nokogiri::HTML(open("https://premierleague.com/tables"))
+    name = doc.search("span.long").first.text
+    binding.pry 
   end
   
 end  
+name = doc.search("span.long").first.text
